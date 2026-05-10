c565087b80226320f58a4b613ba68449d9f11474
ri <dondonq0027@gmail.com>
2026-05-10 13:59:25 +0900

    modified the code and add IF annomly score png

--- train_model.py
+++ train_model.py
@@ -17,8 +17,14 @@ y = df['FLAG']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
 scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)
+X_train_scaled = pd.DataFrame(
+    scaler.fit_transform(X_train), 
+    columns=X_train.columns
+)
+X_test_scaled = pd.DataFrame(
+    scaler.transform(X_test), 
+    columns=X_test.columns
+)
 
 #4 models
 models = {
